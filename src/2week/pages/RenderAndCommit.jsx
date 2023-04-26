ce5fd04526bcf6fbec8daa2c80ccccea997788b5
june6723 <june6723@naver.com>
2023-04-26 10:36:18 +0900

    :adhesive_bandage: import 지우고 코드 조금 수정

--- src/2week/pages/RenderAndCommit.jsx
+++ src/2week/pages/RenderAndCommit.jsx
@@ -1,32 +1,7 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 
 export default function RenderAndCommit() {
-  return (
-    <section>
-      <h1>Inspiring Sculptures</h1>
-      <Image />
-      <Image />
-      <Image />
-      <Counter />
-    </section>
-  );
-}
-
-function Image() {
-  return (
-    <img
-      src="https://i.imgur.com/ZF6s192.jpg"
-      alt="'Floralis Genérica' by Eduardo Catalano: a gigantic metallic flower sculpture with reflective petals"
-    />
-  );
-}
-
-function Counter() {
   const [count, setCount] = useState(0);
-
-  if (count === 1) {
-    setCount(2);
-  }
 
   return (
     <div>
--- src/2week/pages/StateInReact.jsx
+++ src/2week/pages/StateInReact.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { sculptureList } from "../data";
 
 export default function StateInReact() {
