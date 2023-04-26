eadb99d60254a92545523327516c83aa18ebc7c5
v1r4m <choyoung7789@gmail.com>
2023-04-26 14:26:25 +0900

    Challenge2.jsx

--- src/2week/challenge/Challenge2.jsx
+++ src/2week/challenge/Challenge2.jsx
@@ -1,7 +1,19 @@
+//import { useState } from "react";
+
 export function getFinalState(baseState, queue) {
   let finalState = baseState;
-
-  // TODO: do something with the queue...
-
+  //const [stateNumber, setStateNumber] = useState(baseState);
+  // for(var i =0;i<queue.length;i++){
+  //   setStateNumber(queue[i]);
+  // }
+  // finalState = stateNumber;
+  //이건 왜 안되나요...?
+  for(var i = 0;i<queue.length; i++){
+      if(typeof(queue[i]) == 'function'){
+        finalState = queue[i](finalState);
+      }else{
+        finalState = queue[i];
+      }
+  }
   return finalState;
 }
