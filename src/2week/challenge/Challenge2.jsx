2ca4b41c7865a75c6955331b33fc268a0e6c854a
june6723 <june6723@naver.com>
2023-05-03 10:20:54 +0900

    :sparkles: 2주차 challenge 정답

--- src/2week/challenge/Challenge2.jsx
+++ src/2week/challenge/Challenge2.jsx
@@ -1,7 +1,30 @@
-export function getFinalState(baseState, queue) {
-  let finalState = baseState;
+// export function getFinalState(baseState, queue) {
+//   let finalState = baseState;
 
-  // TODO: do something with the queue...
+//   queue.forEach((update) => {
+//     finalState = typeof update === "function" ? update(finalState) : update;
+//   });
 
-  return finalState;
-}
+//   return finalState;
+// }
+
+export const getFinalState = (baseState, queue) =>
+  queue.reduce(
+    (state, update) => (typeof update === "function" ? update(state) : update),
+    baseState
+  );
+
+// import { useEffect, useState } from "react";
+
+// export function getFinalState(baseState, queue) {
+//   const [finalState, setFinalState] = useState(baseState);
+
+//   useEffect(() => {
+//     queue.forEach((update) => setFinalState(update));
+//     return () => {
+//       setFinalState(baseState);
+//     };
+//   }, []);
+
+//   return finalState;
+// }
