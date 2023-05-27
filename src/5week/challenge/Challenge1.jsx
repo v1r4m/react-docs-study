bdde7b4805deef5d6b2dd6661cf0ac2d5ed0a7f0
June Kim <gjune6723@gmail.com>
2023-05-27 17:44:09 +0900

    :sparkles: 5week 문제 풀이

--- src/5week/challenge/Challenge1.jsx
+++ src/5week/challenge/Challenge1.jsx
@@ -1,19 +1,29 @@
 import { useState } from "react";
 import "./style.css";
+import { useRef } from "react";
+import { flushSync } from "react-dom";
 
 export default function CatFriends() {
   const [index, setIndex] = useState(0);
+  const focusedRef = useRef(null);
 
   return (
     <>
       <nav>
         <button
           onClick={() => {
-            if (index < catList.length - 1) {
-              setIndex(index + 1);
-            } else {
-              setIndex(0);
-            }
+            flushSync(() => {
+              if (index < catList.length - 1) {
+                setIndex(index + 1);
+              } else {
+                setIndex(0);
+              }
+            });
+            focusedRef.current.scrollIntoView({
+              behavior: "smooth",
+              block: "nearest",
+              inline: "center",
+            });
           }}
         >
           Next
@@ -22,7 +32,7 @@ export default function CatFriends() {
       <div>
         <ul>
           {catList.map((cat, i) => (
-            <li key={cat.id}>
+            <li key={cat.id} ref={i === index ? focusedRef : null}>
               <img
                 className={index === i ? "active" : ""}
                 src={cat.imageUrl}
